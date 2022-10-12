7dcbecf3458934e5e17e46cf89ba9bce34734f79
hamid2117 <saim008865@gmail.com>
2022-10-12 17:35:15 +0500

    intial commit

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,10 +1,13 @@
 require('dotenv').config()
 require('express-async-errors')
+
 const express = require('express')
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 const productsRouter = require('./routes/products')
+
 const app = express()
+
 //database connection
 require('./db/connect')
 
