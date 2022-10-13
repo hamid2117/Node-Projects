a9de607a9e10755a71a221cad7543f9590074633
hamid2117 <saim008865@gmail.com>
2022-10-13 06:48:16 +0500

    done adding pagination

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -31,6 +31,12 @@ const getProducts = async (req, res) => {
     const fieldsList = fields.split(',').join(' ')
     result = result.select(fieldsList)
   }
+  //Pagination functionality
+  const limit = Number(req.query.limit) || 10
+  const page = Number(req.query.page) || 1
+  const skip = (page - 1) * limit
+
+  result = result.limit(limit).skip(skip)
 
   const products = await result
 
