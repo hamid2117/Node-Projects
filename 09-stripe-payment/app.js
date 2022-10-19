079d354ea43ade9d1bff7700d906d80871263398
hamid2117 <saim008865@gmail.com>
2022-10-19 19:26:57 +0500

    done strip payment

--- 09-stripe-payment/app.js
+++ 09-stripe-payment/app.js
@@ -1,32 +1,35 @@
-require('dotenv').config();
-require('express-async-errors');
+require('dotenv').config()
+require('express-async-errors')
 
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
 // controller
-
+const stripRoute = require('./controllers/stripeController')
 // error handler
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+const { get } = require('mongoose')
 
-app.use(express.json());
-app.use(express.static('./public'));
+app.use(express.json())
+app.use(express.static('./public'))
 
 // stripe
-app.use(notFoundMiddleware);
-app.use(errorHandlerMiddleware);
+app.post('/stripe', stripRoute)
 
-const port = process.env.PORT || 3000;
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+const port = process.env.PORT || 3000
 
 const start = async () => {
   try {
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
-    );
+    )
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
-};
+}
 
-start();
+start()
