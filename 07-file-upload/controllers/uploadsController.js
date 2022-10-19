4e89dbf2378b3c68a555902c57964f70918b71ad
hamid2117 <saim008865@gmail.com>
2022-10-19 11:47:45 +0500

    done error handling in upload image route

--- 07-file-upload/controllers/uploadsController.js
+++ 07-file-upload/controllers/uploadsController.js
@@ -1,9 +1,22 @@
 const Product = require('../models/Product')
 const { StatusCodes } = require('http-status-codes')
+const { BadRequestError } = require('../errors')
 const path = require('path')
 const uploadProductImage = async (req, res) => {
+  if (!req.files) {
+    throw new BadRequestError('No File is uploaded')
+  }
+
   const productImage = req.files.image
 
+  if (!productImage.mimetype.startsWith('image')) {
+    throw new BadRequestError('Please upload Image')
+  }
+  const maxSize = 100000
+
+  if (productImage.size > maxSize) {
+    throw new BadRequestError('please upload image under smaller 100KB')
+  }
   const imagePath = path.join(
     __dirname,
     '../public/uploads/' + `${productImage.name}`
